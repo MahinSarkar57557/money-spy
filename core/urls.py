0915107129f176c6b232c2782dbbef537c8941ec
mahin sarkar <mahinsarkar@mahins-MacBook-Air.local>
2026-07-31 23:58:25 +0600

    added login and logout urls in core project

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # লগইন পেজ পাথ (name='login' থাকা বাধ্যতামূলক)
+    path('login/', auth_views.LoginView.as_view(template_name='tracker/login.html'), name='login'),
+    
+    # লগআউট পেজ পাথ
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    
+    # আপনার অ্যাপের ইউআরএল
     path('', include('tracker.urls')),
 ]
